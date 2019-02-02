a75037e5362b29aad4bd2b4ef1e1aae909f50c3b
freddypage <fredlepage782@gmail.com>
2019-02-02 16:40:50 -0500

    changes

--- tactio-challenge/src/App.js
+++ tactio-challenge/src/App.js
@@ -7,7 +7,18 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h5>Welcome litty bois</h5>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
